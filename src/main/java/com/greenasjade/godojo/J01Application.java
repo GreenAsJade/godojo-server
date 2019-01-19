c10f56482c9e2c15067d4fad2c37922f7b71a788
Martin Gregory <martin.gregory@oasissystems.com.au>
2019-01-19 18:11:37 +1030

    Can edit position description (!)

--- src/main/java/com/greenasjade/godojo/J01Application.java
+++ src/main/java/com/greenasjade/godojo/J01Application.java
@@ -9,121 +9,120 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 
-
 @SpringBootApplication
 public class J01Application {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(J01Application.class);
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(J01Application.class, args);
 	}
-	
+
 	@Bean
 	public CommandLineRunner initialise (
-			BoardPositionStore bp_store, 
+			BoardPositionStore bp_store,
 			JosekiStore j_store,
 			MoveStore m_store) {
 		return args -> {
 			log.info("Initialising...");
-			BoardPosition rootNode = bp_store.findByPlay("root");			
+			BoardPosition rootNode = bp_store.findByPlay("root");
 			if (true) { //rootNode == null) {
 				resetDB(bp_store, j_store, m_store);
 			}
-			rootNode = bp_store.findByPlay("root");			
-	
-			log.info(rootNode.toString());			
+			rootNode = bp_store.findByPlay("root");
+
+			log.info(rootNode.toString());
 		};
 	}
-	
+
 	void resetDB(
-			BoardPositionStore bp_store, 
+			BoardPositionStore bp_store,
 			JosekiStore j_store,
 			MoveStore m_store) {
 		log.info("reseting DB...");
-		
+
 		bp_store.deleteAll();
 		j_store.deleteAll();
 		m_store.deleteAll();
-		
+
 		BoardPosition rootNode = new BoardPosition("root");
 		rootNode.addComment("test comment");
 		rootNode.setTitle("Empty Board");
 		rootNode.setDescription("Infinite possibilities await!");
-		
+
 		bp_store.save(rootNode);
-		
+
 		log.info("After save of root: " + rootNode.toString() );
-		
+
 		Long root_id = rootNode.id;
-		
-		BoardPosition child; 
-	
-		child = rootNode.addMove("Q16");		
+
+		BoardPosition child;
+
+		child = rootNode.addMove("Q16");
 		child = rootNode.addMove("R16");
 		child = rootNode.addMove("R17");
 		child.setTitle("San San");
 		child = rootNode.addMove("K10");
 		child.setTitle("Tengen");
 		child.setDescription("Dwyrin's favourite!");
-		
+
 		bp_store.save(rootNode);
 
 		log.info("After save of root with children: " + rootNode.toString() );
-		
+
 		Move the_move;
-		
-		/* Figuring out how/when/what Neo4j loads */ 
+
+		/* Figuring out how/when/what Neo4j loads */
 		log.info("Loading and looking at child moves...");
- 		
+
 		the_move = rootNode.children.toArray(new Move[0])[0];
 		log.info("After creation, move on the root node: " + the_move.toString() );
-		
+
 		rootNode = bp_store.findByPlay("root");
 		log.info("reloaded root: " + rootNode.toString());
-		
+
 		the_move = rootNode.children.iterator().next();
 		log.info("After reload, move from root: " + the_move.toString() ); // The move doesn't have it's target loaded
-		
+
 		the_move = child.parent;
-		log.info("Child POV, move: " + the_move.toString() );	
-		
+		log.info("Child POV, move: " + the_move.toString() );
+
 		the_move = m_store.findByPlacement("Q16");
 		log.info("Move direct load: " + the_move.toString());
-		
+
 		/* Test reload of a position */
-				
+
 		rootNode = bp_store.findById(root_id).orElse(null);
 		log.info("After findById: " + rootNode.toString() );
-		
+
 		log.info("Adding a joseki...");
 		Joseki j1 = new Joseki("Joseki1");
 
 		the_move = m_store.findByPlacement("Q16");
 		log.info("First move direct load: " + the_move.toString());
-	
+
 		j1.addMove(the_move);
 		j_store.save(j1);
-		
+
 		log.info(j1.toString());
-		
+
 		the_move = m_store.findByPlacement("Q16");
 		log.info("First move direct load: " + the_move.toString());
-			
+
 		the_move = m_store.findByPlacement("R16");
 		log.info("Second move direct load: " + the_move.toString());
 
 		log.info("Adding second level moves to a node...");
-		
+
 		child.addMove("Q16");
 		child.addMove("R16");
 		child.addMove("R17");
 		bp_store.save(child);
-		
+
 		// Test adding a comment later
 		rootNode.addComment("second comment");
 		bp_store.save(rootNode);
-		
+
 		log.info("...DB reset done");
-	}	
+	}
 }
--- src/main/java/com/greenasjade/godojo/PositionDTO.java
+++ src/main/java/com/greenasjade/godojo/PositionDTO.java
@@ -1,16 +1,26 @@
 package com.greenasjade.godojo;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class PositionDTO extends HalResource {
 
     private final String play;
     public String getPlay() {return play;}
-    
-    private final String description;
+
+    private String description;
     public String getDescription() {return description;}
+    public void setDescription(String description) {this.description = description;}
 
     private final String title;
     public String getTitle() {return title;}
-    
+
+    @JsonCreator
+    public PositionDTO(@JsonProperty("description") String description) {
+        this.description = description;
+        this.title="";
+        this.play="";
+    }
     public PositionDTO(BoardPosition position) {
         play = position.getPlay();
         title = position.getTitle();
