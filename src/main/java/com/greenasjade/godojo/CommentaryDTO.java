276488a723254a1693c4ae404b47ab094909fde6
Martin Gregory <martin.gregory@oasissystems.com.au>
2019-02-09 11:37:15 +1030

    Sorted commentary

--- src/main/java/com/greenasjade/godojo/CommentaryDTO.java
+++ src/main/java/com/greenasjade/godojo/CommentaryDTO.java
@@ -1,18 +1,37 @@
 package com.greenasjade.godojo;
 
-import java.util.ArrayList;
+import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+@Data
 public class CommentaryDTO {
 
+    private static final Logger log = LoggerFactory.getLogger(CommentaryDTO.class);
+
     private ArrayList<Comment> commentary;
-    public ArrayList<Comment> getCommentary() {return commentary;}
 
     public String toString() {
         return commentary.toString();
     }
+
+
     // Outbound comment information
     public CommentaryDTO(BoardPosition position) {
         // BoardPositions can have null elements returned from neo4j queries
-        commentary = position.commentary != null ? position.commentary : new ArrayList<>();
+        // BoardPosition commentary array returned from neo is not sorted
+        log.info("Building commentary dto for " + position);
+        if (position.commentary != null) {
+            commentary = position.commentary.stream().
+                    sorted( Comparator.comparing(Comment::getDate))
+                    .collect(Collectors.toCollection(ArrayList::new));
+        }
+        else {
+            commentary =  new ArrayList<>();
+        }
     }
 }
